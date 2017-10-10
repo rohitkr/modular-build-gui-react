a551ae46700eb353d3dd078b9a3104224253e7e5
raushankumarnitdgp <raushankumar.nitdgp@gmail.com>
2017-10-10 13:01:38 +0530

    Object file created

---
+++ server/xt-edge-storage.js
@@ -0,0 +1,16 @@
+var cookieAvailable = {
+    'xt-edge1': {
+        occupied: false,
+        time: 0
+    },
+    'xt-edge2': {
+        occupied: false,
+        time: 0
+    },
+    'xt-edge3': {
+        occupied: false,
+        time: 0
+    }
+};
+
+module.exports = cookieAvailable;
